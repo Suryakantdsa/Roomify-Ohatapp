2f66b4539e6f8e45a53eb8be62414146b14d1741
Surykant das <dassuryakantadas@gmail.com>
2025-01-05 22:46:40 +0530

    added favicon and fix issue in avatar component

--- frontend/src/utils/Avatar.tsx
+++ frontend/src/utils/Avatar.tsx
@@ -4,15 +4,15 @@ import { micah } from "@dicebear/collection";
 const Avatar = ({ seed }: { seed: string }) => {
   // Generate the avatar SVG
   const svg = createAvatar(micah, {
-    seed, // Use a unique identifier like a username or ID
-    radius: 50, // Add rounded corners
-    backgroundColor: ["f0f0f0"], // Optional: Light gray background
+    seed,
+    radius: 40,
+    backgroundColor: ["f0f0f0"],
   });
 
   return (
     <div
       dangerouslySetInnerHTML={{ __html: svg }}
-      className="w-12 h-12 cursor-pointer"
+      className="w-12 h-12 cursor-pointer rounded-full hover:bg-pink-400 p-1"
     />
   );
 };
