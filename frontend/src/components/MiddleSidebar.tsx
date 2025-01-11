fa8f31e6c34a93d3da92803999b28767de78c45c
Suryakant das <dassuryakantadas@gmail.com>
2025-01-11 18:24:30 +0530

    redux toolkit added for state mangement

---
+++ frontend/src/components/MiddleSidebar.tsx
@@ -0,0 +1,17 @@
+  
+
+const MiddleSidebar = () => {
+ 
+
+  return (
+    <aside
+      className={`h-screen bg-white border-r shadow-sm transition-all duration-100 border w-[7%]`}
+    >
+      <nav className="flex flex-col h-full">
+       
+      </nav>
+    </aside>
+  );
+};
+
+export default MiddleSidebar;
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,136 +1,106 @@
+import { LogOutIcon, LucideHome, MessageCircle, Users } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/appStore";
 import {
-  ChevronFirst,
-  GroupIcon,
-  LogOutIcon,
-  LucideHome,
-  MessageCircle,
-} from "lucide-react";
-import { useState } from "react";
+  setActiveMenuIndex,
+  toggleSidebar,
+} from "../store/slices/SidebarSlice";
 
 const Sidebar = () => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const dispatch = useDispatch();
+  const { isCollapsed, activeMenuIndex } = useSelector(
+    (store: RootState) => store.sidebar
+  );
 
-  const toggleSidebar = () => {
-    setIsCollapsed(!isCollapsed);
-  };
+  const menuItems = [
+    { id: 0, icon: <LucideHome />, label: "Home" },
+    { id: 1, icon: <MessageCircle />, label: "Messages" },
+    { id: 2, icon: <Users />, label: "Rooms" },
+  ];
 
   return (
     <aside
-      className={`h-screen bg-white border-r shadow-sm transition-all duration-100 border ${
-        isCollapsed ? "w-[7%]" : "w-[23%]"
+      className={`h-screen bg-white border-r shadow-sm transition-all duration-200 ${
+        isCollapsed ? "w-[5%]" : "w-[20%]"
       }`}
     >
       <nav className="flex flex-col h-full">
         {/* Header Section */}
-        <div className="flex items-center justify-between p-4 border-b">
-          <div className="flex items-center gap-3">
+        <div className="border-b">
+          <div
+            className="flex justify-start gap-x-4 items-center p-4 cursor-pointer"
+            onClick={() => dispatch(toggleSidebar())}
+          >
             <img
-              //   src="roomify_logo.webp"
               src="https://app.chitchat.gg/svgs/logo.svg"
               alt="Roomify Logo"
-              className="w-10 h-10 cursor-pointer "
-              onClick={toggleSidebar}
+              className="w-10 h-10"
             />
-            {!isCollapsed && <h1 className="text-xl font-bold">Roomify</h1>}
+            {!isCollapsed && <span>RoomiFy</span>}
           </div>
-
-          <button
-            onClick={toggleSidebar}
-            className="p-2 rounded transition"
-            aria-label="Toggle Sidebar"
-          >
-            {!isCollapsed && <ChevronFirst />}
-          </button>
         </div>
 
         {/* Menu Section */}
-        <ul className="flex-1 p-3 space-y-3">
-          <li className="group ">
-            <a
-              href="#"
-              className="flex items-start  gap-3 p-3 rounded-lg text-gray-700 transition"
-            >
-              <span className=" w-10 h-10 rounded-full">
-                <LucideHome />
-              </span>
-              {!isCollapsed && <span>Home</span>}
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#"
-              className="flex items-start  gap-3 p-3 rounded-lg text-gray-700 transition"
-            >
-              <span className=" w-10 h-10 ">
-                <MessageCircle />
-              </span>
-              {!isCollapsed && <span>Chat</span>}
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#"
-              className="flex items-start  gap-3 p-3 rounded-lg text-gray-700 transition"
-            >
-              <span className=" w-10 h-10 rounded-full">
-                <GroupIcon />
-              </span>
-              {!isCollapsed && <span>Rooms</span>}
-            </a>
-          </li>
-
-          {/* <li className="group ">
-            <a
-              href="#"
-              className="flex items-start gap-3 p-3 rounded-lg text-gray-700 transition"
-            >
-              <span className=" w-10 h-10 ">
-                <Settings />
-              </span>
-              {!isCollapsed && <span>Settings</span>}
-            </a>
-          </li> */}
+        <ul className="flex-1 p-4 space-y-3">
+          {menuItems.map((item) => (
+            <li key={item.id} className="relative group flex justify-center">
+              <a
+                href="#"
+                onClick={() => dispatch(setActiveMenuIndex(item.id))}
+                className={`flex items-center gap-3 p-3  text-gray-700 transition  
+  
+                ${
+                  isCollapsed
+                    ? "w-12 h-12 rounded-full"
+                    : "w-full rounded-lg justify-start"
+                } 
+                ${
+                  activeMenuIndex === item.id
+                    ? "bg-pink-200 text-pink-700 "
+                    : ""
+                }`}
+              >
+                <span
+                  className={`w-10 h-10 flex justify-center items-center ${
+                    !isCollapsed && activeMenuIndex === item.id
+                      ? "bg-pink-100 rounded-full"
+                      : "rounded-full"
+                  }`}
+                >
+                  {item.icon}
+                </span>
+                {!isCollapsed && (
+                  <span className="text-sm font-medium">{item.label}</span>
+                )}
+              </a>
+              {/* Tooltip */}
+              {isCollapsed && (
+                <span className="absolute left-[120%] top-1/2 transform -translate-y-1/2 bg-gray-700 text-white text-xs py-1 px-2 rounded shadow-md opacity-0 group-hover:opacity-100 transition">
+                  {item.label}
+                </span>
+              )}
+            </li>
+          ))}
         </ul>
 
         {/* User Info Section */}
-
-        <div className="p-4 border-b">
-          {isCollapsed && (
-            <div className="flex items-start gap-3 p-3">
-              <span>
-                <LogOutIcon />
-              </span>
+        <div className="p-4 border-t">
+          <div className="flex items-center gap-3">
+            <div className="w-10 h-10 bg-pink-400 text-white flex justify-center items-center rounded-md">
+              JD
             </div>
-          )}
-          <div
-            className={`leading-tight flex items-center ${
-              !isCollapsed ? "justify-between" : "justify-start"
-            } `}
-          >
-            <div className="bg-pink-400 rounded-md p-3">JD</div>
-
             {!isCollapsed && (
               <div>
                 <h4 className="font-semibold">John Doe</h4>
                 <span className="text-sm text-gray-500">johndoe@gmail.com</span>
               </div>
             )}
-            <div className="text-gray-900 transition">
-              {!isCollapsed && (
-                <span>
-                  <LogOutIcon />
-                </span>
-              )}
-            </div>
           </div>
+          <button className="flex items-center gap-3 mt-4 text-gray-700">
+            <LogOutIcon />
+            {!isCollapsed && <span>Logout</span>}
+          </button>
         </div>
-
-        {/* Footer Section */}
-        {!isCollapsed && (
-          <div className="p-4 border-t text-gray-500 text-sm">
-            &copy; {new Date().getFullYear()} Roomify. All rights reserved.
-          </div>
-        )}
       </nav>
     </aside>
   );
