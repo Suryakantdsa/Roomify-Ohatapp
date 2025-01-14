caf5e28cbc7208c95decba9032f9247ae073ded6
Suryakant das <dassuryakantadas@gmail.com>
2025-01-14 19:13:54 +0530

    chat middle sidebar complted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import MiddleSidebar from "./components/MiddleSidebar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="w-screen flex font-mono ">
       <Sidebar />
+      <MiddleSidebar />
       <Dashboard />
     </div>
   );
